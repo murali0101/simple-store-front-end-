de00fd11216581f49080c8ee90e6fc956f6f9222
murali0101 <muralimv0101@gmail.com>
2022-06-06 23:59:49 +0530

    product page creating done

--- src/redux/ProductReducer/ProductReducer.js
+++ src/redux/ProductReducer/ProductReducer.js
@@ -1,12 +1,13 @@
 import { ADDDATA } from "./ProductAction";
 
 const init = {
-  productData: [],
+    productData: [],
+    totalPage:""
 };
 
 export const ProductReducer = (store = init, { type, payload }) => {
     switch (type) {
-      case ADDDATA:return {...store,productData:payload}
+      case ADDDATA:return {...store,productData:payload.product,totalPage:payload.totalPage}
     default:
       return store;
   }
